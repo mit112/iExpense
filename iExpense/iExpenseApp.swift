0094e8cc34807e1805602da74553eb132a1f0ac7
Mit Kamal Sheth <sheth.mit@northeastern.edu>
2024-01-22 00:41:33 -0500

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Mit Sheth on 1/22/24.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
